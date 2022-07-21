30cd7c65bac0fe82fc331f42d02ff9e0c8dc7a44
Eremeev_Alexander <sanya.eremey@mail.ru>
2022-07-21 09:21:47 +0300

    Updates

---
+++ js/map.js
@@ -0,0 +1,21 @@
+// const map = L.map('leaflet-map')
+//   .setView({
+//     lat: 59.968137,
+//     lng: 30.316272,
+//   }, 10);
+
+// L.tileLayer(
+//   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//   {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   },
+// ).addTo(map);
+
+// const marker = L.marker(
+//   {
+//     lat: 59.968137,
+//     lng: 30.316272,
+//   },
+// );
+
+// marker.addTo(map);
